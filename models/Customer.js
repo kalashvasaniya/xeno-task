bb1912fc28bf57200262bef84822a28fc3c8f11c
kalash <88212870+kalashvasaniya@users.noreply.github.com>
2024-11-11 13:08:43 +0530

    Model update - init

--- models/Customer.js
+++ models/Customer.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
 const customerSchema = new mongoose.Schema({
-    name: String,
-    email: { type: String, unique: true },
-    phone: String,
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
 });
 
 export default mongoose.models.Customer || mongoose.model('Customer', customerSchema);
