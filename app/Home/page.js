762d766a9c76d89b433573de183ee7b3af7ddf91
kalash <88212870+kalashvasaniya@users.noreply.github.com>
2024-11-15 23:32:34 +0530

    Update page.js

--- app/Home/page.js
+++ app/Home/page.js
@@ -16,7 +16,7 @@ const Home = async () => {
 
     const saveCustomerData = async (name, email) => {
         try {
-            const response = await fetch(`https://xeno-task.vercel.app/api/customers`, {
+            const response = await fetch(`/api/customers`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ name, email }),
